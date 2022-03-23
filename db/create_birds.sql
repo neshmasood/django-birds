172c85e0dea77bd358c640ba3df3f2a9248404f6
neshmasood <neshtam@gmail.com>
2022-03-23 00:28:16 -0500

    created migration files, super users,and birds database models

---
+++ db/create_birds.sql
@@ -0,0 +1,10 @@
+INSERT INTO main_app_bird(name, img, age, gender, created_at)
+VALUES ('Cardi', 'https://www.ajc.com/resizer/BQQECXJJxyC5M22qSabyx6g4PiE=/arc-anglerfish-arc2-prod-ajc/public/4JMOGWH2WNA65PDHFKLYLTXY2U.jpg', 10, 'female', '03-19-2022');
+INSERT INTO main_app_bird(name, img, age, gender, created_at)
+VALUES ('Robin', 'https://www.allaboutbirds.org/guide/assets/photo/303441381-480px.jpg', 2, 'male','03-19-2022');
+INSERT INTO main_app_bird(name, img, age, gender, created_at)
+VALUES ('Sparrow', 'https://www.thespruce.com/thmb/c2gJp_P5kFTURcJelh9eeeuM3IA=/1500x1000/filters:fill(auto,1)/house-sparrow-5974a13bd963ac00103febf7.jpg', 5, 'male','03-19-2022');
+INSERT INTO main_app_bird(name, img, age, gender, created_at)
+VALUES ('Woody', 'https://www.allaboutbirds.org/guide/assets/photo/297900561-480px.jpg', 1, 'male','03-19-2022');
+INSERT INTO main_app_bird(name, img, age, gender, created_at)
+VALUES ('Jay', 'https://images.squarespace-cdn.com/content/v1/586479801b631b1efed5deb9/1546105616983-I5AH0RPG8KPEE2ATPOGG/9fc9f712-1e22-4ffd-bd20-bc8baec18f55.jpg?format=1500w', 18, 'female','03-19-2022');
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views import View # View class to handle requests
 from django.http import HttpResponse #This is our responses
+from .models import Bird
 
 # Create your views here.
 
@@ -19,28 +20,50 @@ class About(TemplateView):
     template_name = 'about.html'
     # OLD GET REQ
     # def get(self, request):
-    #     return HttpResponse("Cats About")
-
-class Bird: 
-
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
+    #     return HttpResponse("Birds About")
 
 
-birds = [
-    Bird("Cardi", 10, "Female"),
-    Bird("Robin", 2, "Male"),
-    Bird("Sparrow", 5, "Male"),
-    Bird("Woody", 1, "Male"),
-    Bird("Jay", 18, "Female")
-]
+# ---------------------------------------------------
+# ---------------------------------------------------
+# after database, we no longer need class Bird
+
+# class Bird: 
+
+#     def __init__(self, name, age, gender):
+#         self.name = name
+#         self.age = age
+#         self.gender = gender
+
+
+# birds = [
+#     Bird("Cardi", 10, "Female"),
+#     Bird("Robin", 2, "Male"),
+#     Bird("Sparrow", 5, "Male"),
+#     Bird("Woody", 1, "Male"),
+#     Bird("Jay", 18, "Female")
+# ]
+
+# class Bird_List(TemplateView):
+#     template_name = 'birdlist.html'
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['birds'] = birds # this is where we add the key into our context object for the view to use
+#         return context
+
+# ---------------------------------------------------
+# ---------------------------------------------------
 
 class Bird_List(TemplateView):
     template_name = 'birdlist.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['birds'] = birds # this is where we add the key into our context object for the view to use
+        name = self.request.GET.get("name")
+        if name != None:
+            context['birds'] = Bird.objects.filter(name_icontains=name)
+            context['header'] = f"Searching for {name}"
+        else:
+            context['birds'] = Bird.objects.all()
+            context['header'] = "Birds" # this is where we add the key into our context object for the view to use
         return context
