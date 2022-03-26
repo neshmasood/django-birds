b95bc1ae5e68a92765cbbe8a56d750db244bea42
neshmasood <neshtam@gmail.com>
2022-03-26 13:58:35 -0500

    add birdhouse model migration

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Bird
+from .models import Bird, BirdHouse
 
 # Register your models here.
 
 admin.site.register(Bird)
+admin.site.register(BirdHouse)
--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class BirdHouse(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 GENDER_CHOICES = {
     ("f", "female"), 
     ("m", "male")
@@ -15,6 +23,7 @@ class Bird(models.Model):
     age = models.IntegerField()
     gender = models.CharField(max_length=10, choices = GENDER_CHOICES)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Birdhouse = models.ManyToManyField(BirdHouse) # M:M example
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
