9295fadefc29f52da92167d43b323b6ba9d5e5c2
neshmasood <neshtam@gmail.com>
2022-03-26 17:24:43 -0500

    rename birdhouses and made minor change in models.py to fix bird update function. app working properly now

--- main_app/models.py
+++ main_app/models.py
@@ -23,7 +23,7 @@ class Bird(models.Model):
     age = models.IntegerField()
     gender = models.CharField(max_length=10, choices = GENDER_CHOICES)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    Birdhouse = models.ManyToManyField(BirdHouse) # M:M example
+    birdhouses = models.ManyToManyField(BirdHouse) # M:M example
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
